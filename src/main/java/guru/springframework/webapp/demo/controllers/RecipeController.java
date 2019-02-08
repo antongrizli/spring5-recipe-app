d1e2cbcd688d7a84919846da80bb6b6ed360021e
Anton Konikov <antongrizli@gmail.com>
2019-02-08 17:43:42 +0100

    Finished lesson 237

--- src/main/java/guru/springframework/webapp/demo/controllers/RecipeController.java
+++ src/main/java/guru/springframework/webapp/demo/controllers/RecipeController.java
@@ -69,4 +69,17 @@ public class RecipeController {
 
         return modelAndView;
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView badRequestFound(Exception e) {
+        log.error("Number Format exception");
+        log.error(e.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", e);
+
+        return modelAndView;
+    }
 }
--- src/test/java/guru/springframework/webapp/demo/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/webapp/demo/controllers/RecipeControllerTest.java
@@ -70,6 +70,15 @@ public class RecipeControllerTest {
     }
 
     @Test
+    public void testBadRequest() throws Exception {
+        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/klm/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
+
+    @Test
     public void testPostNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
         command.setId(2L);
